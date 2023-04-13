006029650961c2f010bd7c47d34714b453f69d72
Bharat Pathak <97437398+Bharatpathak08@users.noreply.github.com>
2023-04-13 19:11:13 +0530

    Update edit_profile.php

--- edit_profile.php
+++ edit_profile.php
@@ -7,6 +7,7 @@
 	$email = "";
 	$mobile = "";
 	$address = "";
+
 	$query = "select * from users where email = '$_SESSION[email]'";
 	$query_run = mysqli_query($connection,$query);
 	while ($row = mysqli_fetch_assoc($query_run)){
