b7b191a3947339359a5f6c43bd53f5005d714387
Tanu Kumari <tanukumarisinha.123@gmail.com>
2023-04-19 19:57:16 +0530

    UPDATED_JS

--- js/script.js
+++ js/script.js
@@ -6,49 +6,21 @@ document.querySelector('#menu-btn').onclick = () =>{
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
-
 let searchForm = document.querySelector('.search-form');
-
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
-
 let cartItem = document.querySelector('.cart-items-container');
-
 document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
 }
-
 window.onscroll = () =>{
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
-
-/**
- * navbar toggle
- */
-
-// const navbar = document.querySelector("[data-navbar]");
-// const navbarLinks = document.querySelectorAll("[data-nav-link]");
-// const navToggler = document.querySelector("[data-nav-toggler]");
-
-// const toggleNavbar = function () {
-//   navbar.classList.toggle("active");
-//   navToggler.classList.toggle("active");
-//   document.body.classList.toggle("active");
-// }
-
-// addEventOnElem(navToggler, "click", toggleNavbar);
-
-// const closeNavbar = function () {
-//   navbar.classList.remove("active");
-//   navToggler.classList.remove("active");
-//   document.body.classList.remove("active");
-// }
-
 addEventOnElem(navbarLinks, "click", closeNavbar);
